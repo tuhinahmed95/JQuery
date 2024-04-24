1b16c056f59ffb650c641be5b01dc2dbdaf8d122
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-24 22:24:40 +0600

    jquery selector & manipulation

---
+++ html selector and text manipulation/index.js
@@ -0,0 +1,11 @@
+// multiple element and multiple class & id select
+// $("#para1,.my-div,.head2").text("hi")
+
+// text manipulation and use html method
+// $("#para1").html("<b>hello</b>")
+
+
+// use append & prepend method
+
+// $("#para1").append(" this is append")
+// $("#para1").prepend(" this is prepend")
